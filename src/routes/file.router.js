4e8682919ec658f1cfe4fc97cfbd1aa0a14d8faf
Jayfx24 <176495720+Jayfx24@users.noreply.github.com>
2026-08-11 10:22:42 +0100

    feat(multer): implement error handling for multer

--- src/routes/file.router.js
+++ src/routes/file.router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import multer from "multer";
 import {
   uploadFile,
   getFile,
@@ -10,12 +11,23 @@ import { updateSchema } from "../schemas/update.schema.js";
 import { validateRequest } from "../middleware/validateRequest.js";
 import getBreadcrumbs from "../middleware/breadcrumbs.js";
 import { isAuth } from "../middleware/auth.middleware.js";
+import AppError from "../errors/App.error.js";
+
 const fileRouter = Router();
+
+const u = upload.single("uploaded_file");
 
 fileRouter.post(
   "/:parentId/upload",
   isAuth,
-  upload.single("uploaded_file"),
+  u(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      throw new AppError(err.message, 500, err.field);
+    } else if (err) {
+      throw new AppError(err.message, 500);
+    }
+  }),
   uploadFile
 );
 fileRouter.get("/:id/download", isAuth, download);
